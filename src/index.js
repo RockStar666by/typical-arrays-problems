3906aa796815582a56f41055ad26b8f2520fecd8
RockStar666by <savchuk799@gmail.com>
2020-03-18 12:55:43 +0300

    add: solution for typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array == undefined || array == 0) return 0;
+    let arr = array.sort((a, b) => (a - b));
+    return arr[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+    if (array == undefined || array == 0) return 0;
+    let arr = array.sort((a, b) => (b - a));
+    return arr[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == undefined || array == 0) return 0;
+    let sum = 0;
+    array.forEach(elem => sum += elem);
+    return (sum / array.length);
 }
